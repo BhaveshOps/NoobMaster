23e4cde5ddc00ff2d9d01d5e11b373c9def58eed
BhaveshOps <55738603+BhaveshOps@users.noreply.github.com>
2021-06-25 16:26:51 +0530

    Update WelcomeMessage.java

--- NoobProject1/src/noobproject1/noob1/WelcomeMessage.java
+++ NoobProject1/src/noobproject1/noob1/WelcomeMessage.java
@@ -4,6 +4,7 @@ public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome by Noob User 1");
+		System.out.println("Noob User updated manually in Git Hub");
 	}
 
 }
